557b2adceb474920703f624c2d46245a657add37
선태욱 <96423523+King-Mandoo@users.noreply.github.com>
2022-06-04 19:37:08 +0900

    Add files via upload
    찐 최종

--- 1학년 1학기 기초 HTML 수업/집 만들기 과제/선태욱.js
+++ 1학년 1학기 기초 HTML 수업/집 만들기 과제/선태욱.js
@@ -1,38 +1,39 @@
 var vcanvas, ctx; 
-var pi = pi * Math.PI;
 
-function 선태욱() {
+function 선태욱(x, y) {
     vcanvas = document.getElementById("house"); 
     ctx = vcanvas.getContext("2d");   
     
-    // 집 모양
+    // 값
+    x = 200;  
+    y = 400;
 
     // 집 본체
     ctx.beginPath();
-    ctx.moveTo(200,400); 
-    ctx.lineTo(200,700);
-    ctx.lineTo(800,700);
-    ctx.lineTo(800,400);
-    ctx.lineTo(200,400);
+    ctx.moveTo(x, y); 
+    ctx.lineTo(x, y + 300);
+    ctx.lineTo(x + 600, y + 300);
+    ctx.lineTo(x + 600, y);
+    ctx.lineTo(x, y);
     ctx.fillStyle = "yellow";
     ctx.stroke(); 
     ctx.fill();
 
     // 지붕
     ctx.beginPath();
-    ctx.moveTo(200,400);
-    ctx.lineTo(200,150);
-    ctx.lineTo(700,400);
+    ctx.moveTo(x, y);
+    ctx.lineTo(x, y - 250);
+    ctx.lineTo(x + 500, y);
     ctx.fillStyle = "red";
     ctx.stroke(); 
     ctx.fill();
 
     // 굴뚝
     ctx.beginPath();
-    ctx.moveTo(700,400);
-    ctx.lineTo(700,200);
-    ctx.lineTo(770,200);
-    ctx.lineTo(770,400);
+    ctx.moveTo(x + 500, y);
+    ctx.lineTo(x + 500, y - 200);
+    ctx.lineTo(x + 570, y - 200);
+    ctx.lineTo(x + 570, y);
     ctx.stroke(); 
     ctx.fillStyle = "black";
     ctx.stroke(); 
@@ -40,30 +41,30 @@ function 선태욱() {
 
     // 창문
     ctx.beginPath();
-    ctx.arc(400, 520, 100, 0, 10);
+    ctx.arc(x + 200, y + 120, 100, 0, Math.PI * 2);
     ctx.fillStyle="skyblue";
     ctx.fill();
     ctx.stroke(); 
 
     ctx.beginPath();
-    ctx.moveTo(400,420);  
-    ctx.lineTo(400,620);
-    ctx.moveTo(500,520);
-    ctx.lineTo(300,520);
+    ctx.moveTo(x + 200 , y + 20);  
+    ctx.lineTo(x + 200, y + 220);
+    ctx.moveTo(x + 300, y + 120);
+    ctx.lineTo(x + 100, y + 120);
     ctx.stroke(); 
 
     //문
     ctx.beginPath();
-    ctx.moveTo(650,700);  
-    ctx.lineTo(650,500);
-    ctx.lineTo(750,500);
-    ctx.lineTo(750,700);
+    ctx.moveTo(x + 450, y + 300);  
+    ctx.lineTo(x + 450, y + 100);
+    ctx.lineTo(x + 550, y + 100);
+    ctx.lineTo(x + 550, y + 300);
     ctx.fillStyle = "lightyellow";
     ctx.stroke(); 
     ctx.fill();
 
     ctx.beginPath();
-    ctx.arc(730, 600, 7, 0, 10);
+    ctx.arc(x + 530, y + 200, 7, 0, Math.PI * 2);
     ctx.fillStyle="black";
     ctx.fill();
 }
